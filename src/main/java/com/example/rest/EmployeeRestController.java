6324026b879ee43c3328ad3a321e56eb4b82894b
Yerkebulan Yessenali <yerkebulanyessenali03@gmail.com>
2023-09-26 14:15:13 +0600

    add rest controller

---
+++ src/main/java/com/example/rest/EmployeeRestController.java
@@ -0,0 +1,25 @@
+package com.example.rest;
+
+import com.example.dao.EmployeeDAO;
+import com.example.entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1")
+public class EmployeeRestController {
+    private EmployeeDAO employeeDAO;
+
+    public EmployeeRestController(EmployeeDAO employeeDAO) {
+        this.employeeDAO = employeeDAO;
+    }
+
+    @GetMapping("/employees")
+    public List<Employee> findAll(){
+        return employeeDAO.findAll();
+    }
+}
