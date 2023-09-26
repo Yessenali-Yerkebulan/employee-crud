9348332b22066a78b526408a677d863a50036224
Yerkebulan Yessenali <yerkebulanyessenali03@gmail.com>
2023-09-26 18:57:15 +0600

    add rest endpoint to get single employee

--- src/main/java/com/example/rest/EmployeeRestController.java
+++ src/main/java/com/example/rest/EmployeeRestController.java
@@ -5,6 +5,7 @@ import com.example.entity.Employee;
 import com.example.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,4 +25,15 @@ public class EmployeeRestController {
     public List<Employee> findAll(){
         return employeeService.findAll();
     }
+
+    @GetMapping("/employees/{id}")
+    public Employee find(@PathVariable int id){
+        Employee employee = employeeService.findById(id);
+        if(employee==null){
+            throw new RuntimeException("Employee id not found - "+id);
+        }
+        return employeeService.findById(id);
+    }
+
+
 }
