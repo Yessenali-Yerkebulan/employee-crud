4c2813b94fef5e87367d9e089f364a749afd710b
Yerkebulan Yessenali <yerkebulanyessenali03@gmail.com>
2023-09-26 22:14:13 +0600

    commit

--- src/main/java/com/example/repository/EmployeeRepository.java
+++ src/main/java/com/example/repository/EmployeeRepository.java
@@ -2,6 +2,8 @@ package com.example.repository;
 
 import com.example.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+//@RepositoryRestResource(path="employees")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
