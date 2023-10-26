d4e98b6e70c51ad507840f707bac2a08179dff2b
Yerkebulan Yessenali <yerkebulanyessenali03@gmail.com>
2023-10-26 19:40:32 +0600

    restrict URLs based on roles

--- src/main/java/com/example/security/SecurityConfig.java
+++ src/main/java/com/example/security/SecurityConfig.java
@@ -2,9 +2,14 @@ package com.example.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -30,6 +35,26 @@ public class SecurityConfig {
                 .build();
 
         return new InMemoryUserDetailsManager(john, mary, susan);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.authorizeHttpRequests(configurer->
+                configurer
+                        .requestMatchers(HttpMethod.GET, "/api/v1/employees").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.GET, "/api/v1/employess/**").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.POST, "/api/v1/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.PUT, "/api/v1/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.DELETE, "/api/v1/employees/**").hasRole("ADMIN")
+        );
+
+        //use HTTP basic authentication
+        http.httpBasic(Customizer.withDefaults());
+
+        //disable CROSS SITE REQUEST FORGERY(CSRF)
+        http.csrf(csrf -> csrf.disable());
+
+        return http.build();
     }
 
 }
